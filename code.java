5b1a7f69d752998c149762efe60c9c5c3775289b
buddymba <40807359+buddymba@users.noreply.github.com>
2018-07-03 17:14:08 -0700

    Create code.java
    added code in java

---
+++ code.java
@@ -0,0 +1,9 @@
+/**Hello world **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
